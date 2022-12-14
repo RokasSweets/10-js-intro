1aa2e1f5c54b76313c01de8c39e5c60702e4014c
RokasSweets <sweetsound741@gmail.com>
2022-12-14 10:35:55 +0200

    if

--- js/09-if.js
+++ js/09-if.js
@@ -8,6 +8,10 @@ console.clear();
     naudotini: >, <, <=, >=. ===, !==
     nenaudotini: ==, !=
 
+    loginiai operatoriai:
+    && - and - turi tenkinti visas salygas
+    || - turi tenkinti viena salyga
+
     Sablonai:
     if () {}
     if () {} else {}
@@ -92,4 +96,62 @@ if (typeof suma === 'number') {
     console.log('darau pavedima');
 } else {
     console.log('negausi pavedimo');
+}
+
+
+if ( NaN === NaN) {
+    console.log('NaN lygus');
+} else {
+    console.log('NaN nelygus');
+}
+
+console.clear();
+console.log('CLEAR 105');
+
+// code nesting
+
+const day = 3; 
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else {
+    if (day === 2)
+    console.log('antradienis')
+    else {
+        if (day === 3)
+        console.log('treciadienis')
+    } 
+
+    };
+
+
+// atrakcijonu parkas
+
+const parkasDirba = true;
+const bijauAukscio = true;
+
+if (parkasDirba) {
+    if (bijauAukscio) {
+        console.log('Maaaaaamaaa!');
+    } else {
+        console.log('Einu i parka - bus smagu!');
+    }
+} else {
+    if (bijauAukscio) {
+        console.log('Parkas nedirba - man pasiseke - liksiu sausomis kelnemis.');
+    } else {
+        console.log('Parkas nedirba. Man liudna, nes labai norejai nueiti :(');
+    }
+}
+
+if (parkasDirba && bijauAukscio) {
+    console.log('mamaaaaaaaa!');
+} else if (parkasDirba && !bijauAukscio) {
+    console.log('einu i parka - bus smagu');
+} else if (!parkasDirba && bijauAukscio) {
+    console.log('parkas nedirba man pasiseke');
+} else if (!parkasDirba && !bijauAukscio) {
+    console.log('parkas nedirba man liudna nes norejau nueiti');
+} else {
+    console.log('neimanomas atvejis');
 }
