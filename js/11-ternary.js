76b1e33dad5006b66dd1942382bf33bcc2742a4f
RokasSweets <sweetsound741@gmail.com>
2022-12-22 15:07:37 +0200

    comlex ternary

--- js/11-ternary.js
+++ js/11-ternary.js
@@ -36,3 +36,61 @@ console.log(wallet);
 const msgWin = true;
 const msg = msgWin ? 'sveikinu' : 'ups';
 console.log(msg);
+
+// complex ternary
+
+const a = 0 < 1 
+            ? 2 
+            : 3;
+console.log(a);
+
+const b = 0 > 1 
+            ? 2 
+            : 3;
+console.log(b);
+
+const c = 0 === 1 
+                ? 2 
+                : 3;
+console.log(c);
+
+const d = 0 < 1 
+            ? 2 
+            : 3 
+                ? 4 
+                : 5;
+console.log(d);
+
+const e = 0 > 1 
+            ? 2 
+            : 3 
+                ? 4 
+                : 5;
+console.log(e);
+
+
+const f = 0 < 1 
+            ? 2 
+                ? 3 
+                : 4 
+            : 5;
+console.log(f);
+
+const g = 0 > 1 
+            ? 2 
+                ? 3 
+                : 4 
+            : 5 
+                ? 6 
+                : 7;
+console.log(g);
+
+/*
+if (0 < 1) {
+   a = 2
+} else { 
+    if (3) {
+        a = 4
+    }
+}
+*/
---
+++ js/19-sort.js
@@ -0,0 +1,45 @@
+console.clear();
+
+/*
+SORT - rikiavimas
+jei lyginamos reikses yra:
+    -mazesne (-1)
+    -lygios (0)
+    -didesne (1)
+*/
+
+//const numbers = [10, 2, 8, 4, 6]
+// PALEI ABECELE TIK STRINGO PIRMA RAIDE IR TIK STRINGAI
+const dic = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+
+const dicAZ = [...dic].sort(); // nuo maziau iki daugiau
+console.log(dicAZ);
+const dicZA = [...dic].sort().reverse();
+console.log(dicZA);
+
+//
+
+const num = [10, 2, 8, 1, 4, 15, 6, 23, 45];
+const numAZ = [...num].sort((a, b) => a - b);
+const numZA = [...num].sort((a, b) => b - a);
+console.log(num);
+console.log(numAZ);
+console.log(numZA);
+
+/// pagal amziu
+
+const students = [
+    {name: 'Petras', age: 99, isMarried: true},
+    {name: 'Maryte', age: 89, isMarried: false},
+    {name: 'Simon', age: 7, isMarried: false},
+    {name: 'Alas', age: 47, isMarried: true},
+ ];
+
+ const studentAgeAZ = [...students].sort((a, b) => a.age - b.age);
+ const studentAgeZA = [...students].sort((a, b) => b.age - a.age);
+ console.log(studentAgeAZ);
+ console.log(studentAgeZA);
+/// pagal vardus
+ const studentNameAZ = [...students]
+    .sort((a, b) => a.name < b.name ? -1 : 1);
+    console.log(studentNameAZ);
